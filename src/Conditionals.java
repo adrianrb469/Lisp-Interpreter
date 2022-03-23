5b4b988f4932f9fea6256d62d6340e41c2f7e76f
Adrian R <rod21691@uvg.edu.gt>
2022-03-23 16:32:39 -0600

    Creation of conditionals class

---
+++ src/Conditionals.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+
+public class Conditionals {
+	
+	Stack<Double> operands;
+	HashMap<String, Double> myVars;
+	
+	
+	public Conditionals(HashMap<String, Double> vars) {
+		operands = new Stack<>();
+		myVars = vars;
+	}
+	
+	
+}
